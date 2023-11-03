393cbd8487c8bc672079bff5d0ccca680b9d8de9
wsepesi <sepesi.w@wustl.edu>
2023-11-02 21:24:35 -0500

    first push

--- app/auth.ts
+++ app/auth.ts
@@ -1,14 +1,14 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import StravaProvider from 'next-auth/providers/strava';
 
 export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
   providers: [
-    GitHub({
-      clientId: process.env.OAUTH_CLIENT_KEY as string,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET as string
+    StravaProvider({
+      clientId: process.env.STRAVA_CLIENT_ID as string,
+      clientSecret: process.env.STRAVA_CLIENT_SECRET as string
     })
   ],
   pages: {
